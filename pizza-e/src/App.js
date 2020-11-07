d4e7f8f0f8ceb0228c3a3bead7ecb08b7bb2efcf
Hudson <hudsonrogeriojunior@hotmail.com>
2020-11-07 15:10:14 -0300

    Iniciando base do projeto

---
+++ pizza-e/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Iniciando criação das interfaces</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
