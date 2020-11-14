161c3f3c2bdfd20ac0ff6d6b81184cd48cc036c9
Hudson <ra108756@uem.br>
2020-11-14 16:50:51 -0300

    Adicionando botões e borda na tela de login

--- pizza-e/src/components/LoginComponent.js
+++ pizza-e/src/components/LoginComponent.js
@@ -1,28 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './styles/LoginComponentStyle.css'
+
 const Login = () => {
     
     return(
-        <div>
+        <div className= "blockLogin border border-dark loginDiv">
+            <h1 style={{marginBottom: 50}}>Login</h1>
             <input
+                className="loginInput"
                 type="text"
                 name="CPF"
                 placeholder="CPF/CNPJ"
             >
             </input>
             <input
+                className="loginInput"
                 type="password"
                 name="Senha"
                 placeholder="Digite sua senha"
             >
             </input>
-            <button
-                name="Enviar"
-                placeholder="Enviar 2"
-                onClick= {() => {}}
+            <div
+                style={{flexDirection:"row", display:"flex"}}
             >
-            Enviar 
+            <button className="loginButton"> Cadastrar</button>
+            <button
+                name="Login"
+                onClick= {() => {}}
+                className="loginButton"
+            >
+            Entrar 
             </button>
+            </div>
+            <a href="url"> Esqueci minha senha</a>
         </div>
     );
 
