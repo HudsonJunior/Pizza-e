d896642e639daa7afe42567edf99b875faba08ec
cizesk <cizeskiedson@gmail.com>
2020-11-17 22:28:28 -0300

    Criando paginas e tabelas

--- pizza-e/src/components/ButtonCadastrar.js
+++
@@ -1,8 +0,0 @@
-
-const BotaoCadastrar = () =>{
-    <div>
-        <p>alo</p>
-    </div>
-}
-
-export default BotaoCadastrar;
--- pizza-e/src/components/minhaContaComponents.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MinhaConta = () =>{
-    return(
-        <h1>Página da minha conta</h1>
-    )
-}
-
-export default minhaConta
