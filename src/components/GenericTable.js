38565af07a8abca38c6f1d3bec2e7e75e8941f72
Hudson <hudsonrogeriojunior@hotmail.com>
2020-11-21 22:54:31 -0300

    Adicionando parte de produtos na generic table

--- src/components/GenericTable.js
+++ src/components/GenericTable.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import {useHistory} from  "react-router-dom"
 
 import { Table, Button, InputGroup, FormControl } from "react-bootstrap";
@@ -7,6 +8,8 @@ import { Table, Button, InputGroup, FormControl } from "react-bootstrap";
 import { FiEdit3, FiXCircle, FiPlus,  FiSearch } from "react-icons/fi";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import ReactTooltip from 'react-tooltip';
 
 
 const url = window.location.href.replace("http://localhost:3000/func/", "");
@@ -21,8 +24,23 @@ const GenericTable = ({ data, title }) => {
     {url === "clientes" && (history.push("/"))
     
     }
-    {url === "produtos" && (history.push("/"))
-    
+    {url === "produtos" && (
+        confirmAlert({
+          title: "Escolher tipo",
+          message: "Selecione o tipo de produto que deseja cadastrar",
+          buttons: [
+            {
+              label: "Pizza",
+              onClick: () => history.push("/func/cadastro-produto", {tipo: "Pizza"})
+            },
+            {
+              label: "Normal",
+              onClick: () => history.push("/func/cadastro-produto", {tipo: "Normal"})
+            }
+          ]
+        })
+      )
+
     }
     {url === "estoque" && (history.push("/"))
   
@@ -111,8 +129,53 @@ const GenericTable = ({ data, title }) => {
             ))}
           </>
         )}
+
+        {url === "produtos" && (
+          <>
+          <thead>
+            <tr>
+              <td>Tipo</td>
+              <td>Código</td>
+              <td>Nome</td>
+              <td>Valor</td>
+              <td>Peso</td>
+              <td>Status</td>
+              <td>Valor promocional</td>
+              <td>Início da promoção</td>
+              <td>Fim da promoção</td>
+              <td>Ações</td>
+            </tr>
+            </thead>  
+            {data.map((item) => (
+              <tbody>
+                <tr>
+                  <td>{item.tipo}</td>
+                  <td>{item.codigo}</td>
+                  <td>{item.nome}</td>
+                  <td>{item.valor}</td>
+                  <td>{item.peso}</td>
+                  <td>{item.status}</td>
+                  <td>{item.valorPromicional}</td>
+                  <td>{item.inicioPromo}</td>
+                  <td>{item.fimPromo}</td>
+                  <td>
+                    <Button variant="light" data-tip="Editar" style={{marginRight:7, borderWidth:1, borderColor:"black"}}>
+                    <ReactTooltip />
+                      <FiEdit3 size={20} color="#black" />
+                    </Button>
+                    <Button variant="danger" data-tip="Desativar">
+                    <ReactTooltip />
+                      <FiXCircle size={20} color="#black" />
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </>
+        )}
       </Table>
-      <Button variant="success" onClick={direcionarCadastro}>
+      <Button variant="success" data-tip="Adicionar produto" onClick={direcionarCadastro} style={{alignItems:"center", marginLeft:5}}>
+      <ReactTooltip />
         <FiPlus size={26} color="fff"/>
         Adicionar
       </Button>
